1a300c7e6009e173316b4ce1c022bbe930405af6
Aditya Bansal <adi.bansal241996@gmail.com>
2016-08-27 01:06:57 +0530

    Fixed deployment issue to openshift

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var express = require('express');
 
 var app = express();
 
-app.set('port',process.env.PORT||5000);
+app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 8080));
+app.set('ip',(process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'));
+
 app.use(express.static(__dirname +'/CSS'));
 app.use(express.static(__dirname +'/JS'));
 app.use('/',function(req,res){
 	res.sendFile(__dirname+'/views/index.html');
 });
 
-app.listen(app.get('port'),function(){
-console.log("Node Server running at port "+app.get('port'));
+app.listen(app.get('port'), app.get('ip'), function() {
+  console.log('Node app is running on port', app.get('port'),'On ip ',app.get('ip'));
 });
