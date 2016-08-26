8c65c53166b5de1b82056f10b4e2cce6be2662d8
Aditya Bansal <adi.bansal241996@gmail.com>
2016-08-27 01:39:20 +0530

    Added ajax requesting code

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $("#query_box_send").click(function (da) {
   $.ajax({
-          url: "http://127.0.0.1:8080/query",
+          url: "http://medbot-jokebot.rhcloud.com/query",
         dataType: 'json',
         method: "POST",
         data: { key:$("#query_box").val() }
