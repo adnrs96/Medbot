44256d62fbd07791b0e321a76b2db8f5808cac0b
Aditya Bansal <adi.bansal241996@gmail.com>
2016-08-27 01:39:47 +0530

    Added code to echo message

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 8080));
 app.set('ip',(process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'));
 
 app.use(express.static(__dirname +'/CSS'));
 app.use(express.static(__dirname +'/JS'));
-app.use('/',function(req,res){
+app.post('/query',function (req,res) {
+  console.log(req.body.key);
+  res.json({key:req.body.key});
+});
+app.get('/',function(req,res){
 	res.sendFile(__dirname+'/views/index.html');
 });
 
