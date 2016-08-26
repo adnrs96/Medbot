e347974cd6cc957b83390111d225778b108785a5
Aditya Bansal <adi.bansal241996@gmail.com>
2016-08-27 01:39:05 +0530

    Added ajax requesting code

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-
+$("#query_box_send").click(function (da) {
+  $.ajax({
+          url: "http://127.0.0.1:8080/query",
+        dataType: 'json',
+        method: "POST",
+        data: { key:$("#query_box").val() }
+        })
+        .done(function(Data) {
+        $("#chat_side_bot").html(Data.key);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          alert(textStatus);
+        });
+});
 });
