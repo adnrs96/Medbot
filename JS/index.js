5c325f692cfb04c1522ee955cc6257837e8a50e8
Aditya Bansal <adi.bansal241996@gmail.com>
2016-09-14 00:17:29 +0530

    Added imporved UI code

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,80 @@
-$(document).ready(function(){
-$("#query_box_send").click(function (da) {
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  setTimeout(function() {
+    fakeMessage('Hi there, I\'m MedBot and you?');
+  }, 100);
+});
+
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
   $.ajax({
           url: "http://"+window.location.host+"/query",
         dataType: 'json',
         method: "POST",
-        data: { key:$("#query_box").val() }
+        data: { key:msg }
         })
         .done(function(Data) {
-        $("#chat_side_bot").html(Data.key);
+        fakeMessage(Data.key);
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
           alert(textStatus);
         });
+
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
 });
-});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+
+})
+
+
+function fakeMessage(msg) {
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  $('<div class="message loading new"><figure class="avatar"><img src="WhatsApp Image 2016-09-13 at 4.07.26 PM.jpeg" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="WhatsApp Image 2016-09-13 at 4.07.26 PM.jpeg" /></figure>' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 1000 + (Math.random() * 20) * 100);
+
+}
