f4a6b6828e6efd2997487e7d1860c8a2e3734ae6
KUMAR GAURAV PANDEY <gaurav1911@outlook.com>
2016-09-18 20:08:59 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,93 +1,97 @@
-import infermedica_api, sys, json
-infermedica_api.configure(app_id='f1308345', app_key='abb713ecd3ddecf6106ecf9118bfacde')
+import infermedica_api, sys, json, ast
+infermedica_api.configure(app_id='bc057cc8', app_key='cb1d56dafc45383462d1bcbe754c9033')
 
 possible_conditions = []
 
-
 def read_in_from_server():
-    lines = sys.stdin.readlines()
-    return lines[0].split(",")
+    lines = sys.stdin.readlines()[0]
+    lines = json.loads(lines)
+    return (lines)
+
 
 def read_in_from_terminal(text):
     lines = input(text)
     return lines.split(",")
 
+def convert_to_json(request):
+    r = {}
+    r['case_id'] = request.case_id
+    r['conditions'] = [c for c in request.conditions]
+    r['evaluation_time'] = request.evaluation_time
+    r['extras'] = request.extras
+    r['extras_permanent'] = request.extras_permanent
+    r['lab_tests'] = request.lab_tests
+    r['patient_age'] = request.patient_age
+    r['patient_sex'] = request.patient_sex
+    r['pursued'] = request.pursued
+    r['question'] = {}
+    r['question']['extras'] = request.question.extras
+    r['question']['items'] = [item for item in request.question.items]
+    r['question']['text'] = request.question.text
+    r['question']['type'] = request.question.type
+    r['risk_factors'] = request.risk_factors
+    r['symptoms'] = request.symptoms
+    return r
+
 def diagnose(lines, api, request_diag):
+
     for line in lines:
-        #print(line)
-        srch = api.search(line)
-        request_diag.add_symptom(srch[0]['id'], 'present')
+        request_diag.add_symptom(line['id'],line['choice_id']);
 
-    #request_diag.add_symptom(srch[0]['id'], 'present')
     request_diag = api.diagnosis(request_diag)
-    show_res_from_api(request_diag)
-    q_and_a(api, request_diag)
-
-def show_res_from_api(request_diag):
-    print(request_diag.question.text)
-    print("Options:")
-    for item in request_diag.question.items:
-        print("  ",item['name'])
-
-def q_and_a(api, request_diag):
-    stop = 0
-    new_symptoms = []
-    while not stop:
-        #lines = read_in_from_terminal("Answer: ")
-        lines = read_in_from_server()
-        for line in lines:
-            if line == "exit":
-                stop = 1
-                break
-            for item in request_diag.question.items:
-                if line == item['name']:
-                    new_symptoms.append(item['id'])
-
-        for item in request_diag.question.items:
-            if item['id'] in new_symptoms:
-                request_diag.add_symptom(str(item['id']), "present")
-            else:
-                request_diag.add_symptom(str(item['id']), "absent")
-
-        request_diag = api.diagnosis(request_diag)
-        if not stop:
-            show_res_from_api(request_diag)
-
-    possible_conditions[:] = []
-    for c in request_diag.conditions:
-        possible_conditions.append((c['name'],c['probability'],c['id']))
-        if len(possible_conditions) >= 5:
-            break
-    print(possible_conditions)
-    print("Hope it helped :)")
+    request = convert_to_json(request_diag)
+    request = json.dumps(request)
+    print(request)
 
 def main():
-
     api = infermedica_api.get_api()
     request_diag = infermedica_api.Diagnosis(sex='male', age=35)
-    #lines = read_in_from_terminal("Symptom: ")
     lines = read_in_from_server()
     #print(lines)
-    #lines = lines.split(",")
-    #print(linapies)
-    #print(len(request_diag.symptoms))
-    #if len(request_diag.symptoms) <= 0:
     diagnose(lines, api, request_diag)
 
-        #for line in lines:
-        #    srch = api.search(line)
-        #    print(srch[0]['name'])
-        #    request_diag.add_symptom(srch[0]['id'], 'present')
+#def show_res_from_api(request_diag):
+#    print(request_diag.question.text)
+#    print("Options:")
+#    for item in request_diag.question.items:
+#        print("  ",item['name'])
+#
+#def q_and_a(api, request_diag):
+#    print("q and a")
+#    stop = 0
+#    new_symptoms = []
+#    while not stop:
+#        #lines = read_in_from_terminal("Answer: ")
+#        #print(len(sys.stdin.readlines()))
+#        lines = read_in_from_server()
+#        for line in lines:
+#            if line == "exit":
+#                stop = 1
+#                break
+#            for item in request_diag.question.items:
+#                if line == item['name']:
+#                    new_symptoms.append(item['id'])
+#
+#        for item in request_diag.question.items:
+#            if item['id'] in new_symptoms:
+#                request_diag.add_symptom(str(item['id']), "present")
+#            else:
+#                request_diag.add_symptom(str(item['id']), "absent")
+#
+#        request_diag = api.diagnosis(request_diag)
+#        if not stop:
+#            show_res_from_api(request_diag)
+#
+#    possible_conditions[:] = []
+#    for c in request_diag.conditions:
+#        possible_conditions.append((c['name'],c['probability'],c['id']))
+#        if len(possible_conditions) >= 5:
+#            break
+#    print(possible_conditions)
+#    print("Hope it helped :)")
+#
 
-        ##request_diag.add_symptom(srch[0]['id'], 'present')
-        #request_diag = api.diagnosis(request_diag)
-        ##show_res_from_api(request_diag)
-        #print(request_diag.question.text)
-        #print("Options:")
-        #for item in request_diag.question.items:
-        #    print("  ",item['name'])
-    #else:
-    #     q_and_a(api, request_diag)
+
 
 if __name__ == '__main__':
     main()
