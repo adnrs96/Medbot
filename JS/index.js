7ca983ee89213d750533c04b5690877e64508589
Nayan Gupta <nayangupta824@gmail.com>
2016-09-19 17:47:40 +0530

    new session clear chat

--- JS/index.js
+++ JS/index.js
@@ -54,10 +54,13 @@ $('.message-submit').click(function() {
   insertMessage();
 });
 $('.message-submit2').click(function() {
-  $('.messages').empty();
+  /*$('.messages').empty();
+  $('.messages').append('<div class="messages-content"></div>');
    $messages.mCustomScrollbar();
- $('<div class="messages-content mCustomScrollbar _mCS_1 mCS_no_scrollbar"><div id="mCSB_1" class="mCustomScrollBox mCS-light mCSB_vertical mCSB_inside" style="max-height: none;" tabindex="0"><div id="mCSB_1_container" class="mCSB_container mCS_y_hidden mCS_no_scrollbar_y" style="position:relative; top:0; left:0;" dir="ltr"><div class="message new"><figure class="avatar"><img src="WhatsApp Image 2016-09-13 at 4.07.26 PM.jpeg"></figure>Hi again !!<div class="timestamp">17:2</div></div></div><div id="mCSB_1_scrollbar_vertical" class="mCSB_scrollTools mCSB_1_scrollbar mCS-light mCSB_scrollTools_vertical" style="display: none;"><div class="mCSB_draggerContainer"><div id="mCSB_1_dragger_vertical" class="mCSB_dragger" style="position: absolute; min-height: 30px; top: 0px; height: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar" style="line-height: 30px;"></div></div><div class="mCSB_draggerRail"></div></div></div></div></div>').appendTo($('.messages'));
- 
+ setTimeout(function() {
+    fakeMessage('Hi there, I\'m MedBot you own virtual healthcare assistant');
+  }, 100);*/
+ location.reload();
  startnewfunc();
 });
 $(window).on('keydown', function(e) {
